33b34ee9e79a14fd7cc983d5fccff4fd8ae46b5b
bright <brightchukz46@gmail.com>
2025-07-17 13:00:26 +0100

    Update flaskapp03

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 import random
 
 app = Flask(__name__)
 
-quotes = [
-    "Life can be unfair but we face it regardless",
-    "Code is like humor. When you have to explain it, it’s bad.",
-    "Simplicity is the soul of efficiency."
+funny_images = [
+    "funny1.jpg",
+    "funny2.jpg",
+    "funny3.jpg",
+    "funny4.jpg"
 ]
 
-@app.route('/quote')
-def quote():
-    return random.choice(quotes)
+@app.route('/')
+def home():
+    selected_image = random.choice(funny_images)
+    return render_template('funny.html', image=selected_image)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
